da21244eff7882af438c14531309acf4131782e9
Sourodeep Chatterjee <sourodeep224@outlook.com>
2020-08-09 16:01:46 +0530

    Added Loading Component

---
+++ src/components/Loading.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Progress } from "@chakra-ui/core";
+
+const Loading: React.FC = () => {
+	return <Progress hasStripe isAnimated />;
+};
+
+export default Loading;
